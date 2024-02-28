14bae7c6ff9c98921029d1cf47419d02ed1d9506
Ayub40 <ayubk4028@gmail.com>
2024-02-29 01:13:00 +0600

    added show all

--- js/phone.js
+++ js/phone.js
@@ -1,32 +1,39 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = phones => {
+//  --------------------------------------------------------------------------------------
+
+const displayPhones = (phones, isShowAll) => {
     console.log(phones);
     // step-->1
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container cards before adding new cards
     phoneContainer.textContent = '';
 
+    //  --------------------------------------------------------------------------------------
+
     // display show all button if there are more 
     const showAllContainer = document.getElementById('show-all-container')
-    if (phones.length > 12) {
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden')
     }
     else {
         showAllContainer.classList.add('hidden');
     }
+    console.log('is show all', isShowAll);
+    // console.log(phones.length) 
+    // display te koita item dekhabe seta dekhar jonno ei code    
+    // display only first 12 phones if not show all
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
+    }
 
-    // console.log(phones.length)
-    // display te koita item dekhabe seta dekhar jonno ei code
-    // display only first 12 phones
-    phones = phones.slice(0, 12);
-
+    //  --------------------------------------------------------------------------------------
     phones.forEach(phone => {
         // console.log(phone);
         //step--> 2 create a div
@@ -37,7 +44,7 @@ const displayPhones = phones => {
         <figure><img src="${phone.image}"
         alt="Shoes" /></figure>
         <div class="card-body">
-            <h2 class="card-title">${phone.phone_name}</h2>
+            <h2 class="card-title">${phone.phone_name}</h2> 
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-end">
                 <button class="btn btn-primary">Buy Now</button>
@@ -47,32 +54,33 @@ const displayPhones = phones => {
         // step--> append child
         phoneContainer.appendChild(phoneCard);
     });
-
+    //  --------------------------------------------------------------------------------------
     // hide loading spinner
     toggleLoadingSpinner(false);
 }
 
 
 // handle search button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true)
     // console.log('search handle')
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
     console.log(searchText);
-    loadPhone(searchText);
+    loadPhone(searchText, isShowAll);
 }
 
+//  --------------------------------------------------------------------------------------
 
 // handle search recap
 // another search button
 
-const handleSearch2 = () => {
-    toggleLoadingSpinner(true);
-    const searchField = document.getElementById('search-field2')
-    const searchText = searchField.value;
-    loadPhone(searchText);
-}
+// const handleSearch2 = () => {
+//     toggleLoadingSpinner(true);
+//     const searchField = document.getElementById('search-field2')
+//     const searchText = searchField.value;
+//     loadPhone(searchText);
+// }
 
 
 const toggleLoadingSpinner = (isLoading) => {
@@ -85,5 +93,11 @@ const toggleLoadingSpinner = (isLoading) => {
     }
 }
 
+//  --------------------------------------------------------------------------------------
+
+// handle show all
+const handleShowAll = () => {
+    handleSearch(true);
+}
 
 // loadPhone();
