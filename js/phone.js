93cd0a55a830e620a37725c630e6f089d3418f4f
Ayub40 <ayubk4028@gmail.com>
2024-02-28 20:00:05 +0600

    added 2 button

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -7,15 +7,31 @@ const loadPhone = async () => {
 }
 
 const displayPhones = phones => {
-    // console.log(phones);
+    console.log(phones);
     // step-->1
     const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
+
+    // display show all button if there are more 
+    const showAllContainer = document.getElementById('show-all-container')
+    if (phones.length > 12) {
+        showAllContainer.classList.remove('hidden')
+    }
+    else {
+        showAllContainer.classList.add('hidden');
+    }
+
+    // console.log(phones.length)
+    // display te koita item dekhabe seta dekhar jonno ei code
+    // display only first 12 phones
+    phones = phones.slice(0, 12);
 
     phones.forEach(phone => {
-        console.log(phone)
+        // console.log(phone);
         //step--> 2 create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
         // step--> 3: set inner html
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}"
@@ -33,6 +49,20 @@ const displayPhones = phones => {
     })
 
 }
+// handle search button
+const handleSearch = () => {
+    // console.log('search handle')
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
 
+// another search button
+const handleSearch2 = () => {
+    const searchField = document.getElementById('search-field2')
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
 
-loadPhone();
+// loadPhone();
